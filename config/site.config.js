b197cc43e79dc683c153acdd3236314c2f655b98
hanoifishmarket <146512929+hanoifishmarket@users.noreply.github.com>
2024-08-04 23:48:24 -0400

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/Vinfall/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a> with ❤ by Vinfall.',
+    '',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
@@ -55,14 +55,6 @@ module.exports = {
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
-    {
-      name: 'Blog',
-      link: 'https://blog.vinfall.com',
-    },
-    {
-      name: 'GitHub',
-      link: 'https://github.com/Vinfall',
-    },
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
